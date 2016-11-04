8ea71cc259c903f51fcdc130443fa27b4e7e519d
杨晓晨 <xiaochenyung@gmail.com>
2016-11-04 10:15:59 +0800

    a

--- iOS/socketdemo/ViewController.m
+++ iOS/socketdemo/ViewController.m
@@ -9,6 +9,11 @@
 #import <netdb.h>
 #import "ViewController.h"
 #import "GCDAsyncSocket.h"
+//#include <arpa/inet.h>
+#include <net/if.h>
+#include <ifaddrs.h>
+//#import <ifaddrs.h>
+#import <arpa/inet.h>
 @interface ViewController ()<GCDAsyncSocketDelegate>
 @property (nonatomic ,strong) GCDAsyncSocket *socket;
 @end
@@ -18,6 +23,63 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSString *local = [self getIpAddresses];
+    NSLog(@"%@",local);
+}
+
+- (NSString *)localIPAddress
+{
+    NSString *localIP = nil;
+    struct ifaddrs *addrs;
+    if (getifaddrs(&addrs)==0) {
+        const struct ifaddrs *cursor = addrs;
+        while (cursor != NULL) {
+            if (cursor->ifa_addr->sa_family == AF_INET && (cursor->ifa_flags & IFF_LOOPBACK) == 0)
+            {
+                //NSString *name = [NSString stringWithUTF8String:cursor->ifa_name];
+                //if ([name isEqualToString:@"en0"]) // Wi-Fi adapter
+                {
+                    localIP = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)cursor->ifa_addr)->sin_addr)];
+                    break;
+                }
+            }
+            cursor = cursor->ifa_next;
+        }
+        freeifaddrs(addrs);
+    }
+    return localIP;
+}
+
+//获取ip地址
+- (NSString *)getIpAddresses{
+    NSString *address = @"error";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    // retrieve the current interfaces - returns 0 on success
+    success = getifaddrs(&interfaces);
+    if (success == 0)
+    {
+        // Loop through linked list of interfaces
+        temp_addr = interfaces;
+        while(temp_addr != NULL)
+        {
+            if(temp_addr->ifa_addr->sa_family == AF_INET)
+            {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"])
+                {
+                    // Get NSString from C String
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                }
+            }
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    // Free memory
+    freeifaddrs(interfaces);
+//    return [address UTF8String];
+    return address;
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
